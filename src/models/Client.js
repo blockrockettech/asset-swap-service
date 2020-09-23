3bd4e239df9a7fd74504c6526ac6903902a90d81
Vincent de Almeida <vincent@decentralisedengineeringltd.co.uk>
2020-09-23 15:29:35 +0100

    Better naming of postgres client service

--- src/models/Client.js
+++ src/models/Client.js
@@ -1,4 +1,4 @@
 import { Pool } from 'pg';
 import { postgresEndpoint } from '../settings';
 
-export const pool = new Pool({ connectionString: postgresEndpoint });
+export const client = new Pool({ connectionString: postgresEndpoint });
--- src/models/Quote.js
+++ src/models/Quote.js
@@ -1,19 +1,19 @@
-import { pool } from './pool';
+import { client } from './Client';
 
 class Quote {
     constructor() {
-        this.pool = pool;
+        this.client = client;
         this.table = 'quote';
-        this.pool.on('error', (err, client) => `Error, ${err}, on idle client${client}`);
+        this.client.on('error', (err, client) => `Error, ${err}, on idle client${client}`);
     }
 
     async getAll(chainId) {
-        const queryResult = await this.pool.query(`SELECT * FROM ${this.table} WHERE chain_id = ${chainId}`);
+        const queryResult = await this.client.query(`SELECT * FROM ${this.table} WHERE chain_id = ${chainId}`);
         return queryResult.rows;
     }
 
     async addQuote(quoteId, channelId, chainId, input, output, amount, fee) {
-        return this.pool.query(
+        return this.client.query(
             `
                 INSERT INTO quote(quote_id, channel_id, chain_id, input, output, amount, fee)
                 VALUES('${quoteId}', '${channelId}', ${chainId}, '${input}', '${output}', ${amount}, ${fee})
