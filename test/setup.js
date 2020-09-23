8743e894ff89b5c5a1e6969cff3311d1b019ab2f
Vincent de Almeida <vincent@decentralisedengineeringltd.co.uk>
2020-09-23 15:13:53 +0100

    tests with mock DB calls

--- test/setup.js
+++ test/setup.js
@@ -1,11 +1,14 @@
+import _sinon from 'sinon';
 import supertest from 'supertest';
 import chai from 'chai';
 import sinonChai from 'sinon-chai';
 import app from '../src';
 
 chai.use(sinonChai);
+
 export const { expect } = chai;
 export const server = supertest.agent(app);
+export const sinon = _sinon;
 
 // URL exports
 export const INFO_URL = '/';
